6f6c22e2b60d26df390f45072ebb86df372b20a8
Akira Na Thalang <nathalang_t@hotmail.ca>
2025-04-13 16:42:56 -0400

    layout fixes

--- src/Components/MainContent.tsx
+++ src/Components/MainContent.tsx
@@ -1,4 +1,4 @@
-import { motion } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import { useRef, useState } from "react";
 
 import useMousePosition from "./utils/hooks/useMousePosition";
