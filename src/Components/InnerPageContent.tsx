bba4529dcbecd3e27b604e82682def990a22db75
Akira Na Thalang <nathalang_t@hotmail.ca>
2025-04-15 13:55:50 -0400

    Implement parallax scroll effect for intro page

--- src/Components/InnerPageContent.tsx
+++ src/Components/InnerPageContent.tsx
@@ -4,11 +4,37 @@ import IntroSection from "./IntroSection/IntroSection";
 import ToolsCard from "./ToolsSection/ToolsCard";
 
 import classes from "../main.module.scss";
+import { useRef } from "react";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 interface InnerPageContentProps {
   handleMouseEnter: () => void;
   handleMouseLeave: () => void;
 }
+
+const ParallaxSection = ({
+  children,
+  yRange = ["0px", "-30px"],
+}: {
+  children: React.ReactNode;
+  yRange: [string, string];
+}) => {
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({ target: ref });
+
+  // The parallax effect, slowing down the movement
+  const y = useTransform(scrollYProgress, [0, 1], yRange);
+  const padding = useTransform(scrollYProgress, [0, 1], ["0px", "100px"]); // Add some padding as you scroll
+
+  return (
+    <motion.div
+      ref={ref}
+      style={{ y, paddingTop: padding, position: "sticky", top: 0 }}
+    >
+      {children}
+    </motion.div>
+  );
+};
 
 const InnerPageContent = ({
   handleMouseEnter,
@@ -20,10 +46,12 @@ const InnerPageContent = ({
         <HeroWrapper />
         <div className={classes.cardContainer}>
           <div>
-            <IntroSection
-              onMouseLeave={handleMouseLeave}
-              onMouseEnter={handleMouseEnter}
-            />
+            <ParallaxSection yRange={["0px", "-200px"]}>
+              <IntroSection
+                onMouseLeave={handleMouseLeave}
+                onMouseEnter={handleMouseEnter}
+              />
+            </ParallaxSection>
             <ToolsCard />
             <Description
               onMouseLeave={handleMouseLeave}
